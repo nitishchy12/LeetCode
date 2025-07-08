60051462c555bdc4d6d20f115b2bbe58d0252336
Nitish Kumar Choudhary <118154340+nitishchy12@users.noreply.github.com>
2025-07-08 13:22:54 +0530

    Time: 0 ms (100%), Space: 11.4 MB (65.9%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n=digits.size();
+        int i=n-1;
+       while(i>=0 && digits[i]==9 ){
+        digits[i--]=0;
+       }
+       if(i<0){
+        digits.insert(digits.begin(),1);
+       }
+       else{
+        digits[i]++;
+       }
+       return digits;
+
+    }
+};
