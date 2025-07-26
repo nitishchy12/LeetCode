d03e32e0edc7ad841c93c3431fbbf15763cd8423
Nitish Kumar Choudhary <118154340+nitishchy12@users.noreply.github.com>
2025-07-26 11:46:55 +0530

    Time: 0 ms (100%), Space: 29.6 MB (38.12%) - LeetHub

---
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxSum(vector<int>& nums) {
+        unordered_set<int>s;
+        int maxval=INT_MIN;
+        bool ispos=false;
+        for(int n:nums){
+            maxval=max(maxval,n);
+            if(n>0){
+                ispos=true;
+                s.insert(n);
+            }
+        }
+        if(!ispos){
+            return maxval;
+        }
+        int sum=0;
+        for(int x:s){
+           sum+=x;
+        }
+        return sum;
+    }
+};
