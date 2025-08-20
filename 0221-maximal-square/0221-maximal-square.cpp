0116426f048eb2cd0a5da3a344126101b40af9c8
Nitish Kumar Choudhary <118154340+nitishchy12@users.noreply.github.com>
2025-08-20 22:15:58 +0530

    Time: 16 ms (28.9%), Space: 31.2 MB (16.12%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -5,12 +5,16 @@ public:
         if(m==0)
         return 0;
         int n=matrix[0].size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        vector<vector<int>>dp(m,vector<int>(n,0));
         int maxside=0;
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(matrix[i-1][j-1]=='1'){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]=='1'){
+                    if(i==0 || j==0)
+                    dp[i][j]=1;
+                    else
                     dp[i][j]=min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]})+1;
+                    
                     maxside=max(maxside,dp[i][j]);
                 }
             }
