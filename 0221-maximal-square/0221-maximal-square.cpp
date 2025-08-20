2e711dc98bf59eccdd6fa4c799a41e0109f693f3
Nitish Kumar Choudhary <118154340+nitishchy12@users.noreply.github.com>
2025-08-20 22:38:01 +0530

    Time: 8 ms (64.89%), Space: 31.1 MB (36.1%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -9,18 +9,16 @@ public:
         int maxside=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(matrix[i][j]=='1'){
-                    if(i==0 || j==0)
+                if(matrix[i][j]=='1')
+                if(i==0 || j==0){
                     dp[i][j]=1;
-                    else
-                    dp[i][j]=min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]})+1;
-                    
-                    maxside=max(maxside,dp[i][j]);
                 }
+                else{
+                    dp[i][j]=1+min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
+                }
+                maxside=max(maxside,dp[i][j]);
             }
-            
         }
         return maxside*maxside;
-
     }
 };
