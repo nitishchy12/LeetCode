42953993b26ca128ad7ecf2fd18984a3a4b058bf
Nitish Kumar Choudhary <118154340+nitishchy12@users.noreply.github.com>
2025-08-30 15:37:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
-         k=k%n;
-         if(k==0)
-         return ;
+        k=k%n;
+        if(k==0)
+        return 0;
         int temp[k];
         for(int i=0;i<k;i++){
             temp[i]=nums[n-k+i];
