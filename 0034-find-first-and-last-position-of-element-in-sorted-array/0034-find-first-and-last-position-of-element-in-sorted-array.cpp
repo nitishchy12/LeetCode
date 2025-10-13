2e7427a7048dcaeb3e82270931a93bbc9932326b
Nitish Kumar Choudhary <118154340+nitishchy12@users.noreply.github.com>
2025-10-13 21:52:14 +0530

    Time: 0 ms (100%), Space: 17.4 MB (99.33%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,35 +1,51 @@
 class Solution {
-public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int n=nums.size();
-        vector<int>res(2,-1);
+    int firstocc(vector<int>& arr,int k){
+        int n=arr.size();
         int low=0,high=n-1;
         while(low<=high){
-          int mid=low+(high-low)/2;
-          if(nums[mid]<target){
-            low=mid+1;
-          }
-          else{
-            if(nums[mid]==target){
-                res[0]=mid;
+            int mid=(low+high)/2;
+            if(arr[mid]<k){
+                low=mid+1;
             }
-            high=mid-1;
-        }
-        }
-
-        low=0,high=n-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]>target){
+            else if(arr[mid]>k){
                 high=mid-1;
             }
             else{
-              if(nums[mid]==target){
-                res[1]=mid;
-              }
-              low=mid+1;
+                if(mid==0 || arr[mid-1]!=arr[mid]){
+                    return mid;
+                }
+                else{
+                    high=mid-1;
+                }
             }
         }
-        return res;
+        return -1;
+    }
+
+    int lastocc(vector<int>& Arr,int x){
+        int n=Arr.size();
+        int low=0,high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(Arr[mid]>x){
+                high=mid-1;
+            }
+            else if(Arr[mid]<x){
+                low=mid+1;
+            }
+            else{
+                if(mid==n-1 || Arr[mid]!=Arr[mid+1]){
+                    return mid;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+        }
+        return -1;
+    }
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+       return {firstocc(nums,target),lastocc(nums,target)};
     }
 };
