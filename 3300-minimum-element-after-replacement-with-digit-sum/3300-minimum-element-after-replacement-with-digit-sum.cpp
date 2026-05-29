b07661f902e27a0dd7b468a1e4edb868c99fd28d
Nitish Kumar Choudhary <118154340+nitishchy12@users.noreply.github.com>
2026-05-29 12:21:12 +0530

    Time: 0 ms (100%), Space: 28.4 MB (6.96%) - LeetHub

--- 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.cpp
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int minElement(vector<int>& nums) {
-        vector<int>sum;
+        int minVal=INT_MAX;
         for(int x:nums){
+            string s=to_string(x);
             int ds=0;
-            int n=x;
-
-        while(n){
-            ds+=n%10;
-            n/=10;
-        }
-        sum.push_back(ds);
-        }
-        int res=*min_element(sum.begin(),sum.end());
-        return res;
+            for(char c:s){
+                ds+=(c-'0');
+            }
+                minVal=min(minVal,ds);
+            }
+        return minVal;
     }
 };
